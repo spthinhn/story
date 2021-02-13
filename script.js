0d0f28d0d6497a6c824c97cf9242cd258693f743
thinhn <cocungkhongcho@gmail.com>
2021-02-13 15:28:28 +0700

    update css

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+function backPage(page) {
+  var pages = document.getElementsByClassName('page');
+  for(var i = page-2; i < pages.length; i++) {
+    pages[i].classList.remove('flipped');
+  }
+}
+
+function removeFlipped() {
+  var pages = document.getElementsByClassName('page');
+  for(var i = 0; i < pages.length; i++) {
+    pages[i].classList.remove('flipped');
+  }
+}
+
+function addFlipped(page) {
+ var pages = document.getElementsByClassName('page');
+  for(var i = 0; i < (page+1); i++) {
+    pages[i].classList.add('flipped');
+  }
+}
+
+
 function initPageActions() {
   var pages = document.getElementsByClassName('page');
     for(var i = 0; i < pages.length; i++)
@@ -14,19 +36,28 @@ function initPageActions() {
       pages[i].pageNum = i + 1;
       pages[i].onclick=function()
         {
+          if (this.pageNum == 2 || (this.pageNum+1) == end) {
+            document.getElementById("pages").classList.remove('shadow');
+          } else {
+            document.getElementById("pages").classList.add('shadow');
+          }
+          console.log(this.pageNum)
+          if ((this.pageNum+1) == end) {
+            document.getElementById("end").classList.add('shadow');
+          }
           if (this.pageNum % 2 === 0)
             {
-              this.classList.remove('flipped');
-              this.previousElementSibling.classList.remove('flipped');
+              backPage(this.pageNum);
             }
           else
             {
-              this.classList.add('flipped');
-              this.nextElementSibling.classList.add('flipped');
+              removeFlipped();
+              addFlipped(this.pageNum);
             }
          }
       }
 }
+
 
 function loadPage(page) {
   fetch("/pages/"+page+".html").then(function(response) {
@@ -34,6 +65,9 @@ function loadPage(page) {
   }).then(function(string) {
     var elem = document.getElementById("pages");
     elem.insertAdjacentHTML( 'beforeend',string);
+    if (page == first) {
+      document.getElementById("start").classList.add('shadow');
+    }
     if (page == end) {
       initPageActions();
     } else {
